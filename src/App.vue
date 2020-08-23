d184599a02c08a47f5dce93aa8bd621543fb69f5
Teodor Bran <teodor.bran@ic-consult.de>
2020-08-23 16:40:36 +0200

    routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header />
-    <AuthForm />
+    <router-view></router-view>
     <footer />
   </div>
 </template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import AuthForm from "./views/AuthForm.vue";
+import Vue from "vue"
+import VueRouter from "vue-router"
+import AuthForm from "./views/AuthForm.vue"
 
 Vue.use(VueRouter);
 
