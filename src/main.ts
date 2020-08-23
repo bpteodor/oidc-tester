6c02216540dba451fb17aae890aec1f73f23d26b
Teodor Bran <teodor.bran@ic-consult.de>
2020-08-23 16:21:29 +0200

    css

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-import "./custom.css";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "./assets/custom.scss";
 
 Vue.config.productionTip = false;
 
